cd96f509ba86af29dd8fc45bad204ec860230780
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2022-12-11 17:20:29 +0300

    About + Destinations Entites / Added+Created

---
+++ EntityLayer/Concrete/About.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class About
+    {
+        public int AboutID { get; set; }
+        public string AboutDetails { get; set; }
+        public string AboutImage { get; set; }
+        public bool Status { get; set; }
+    }
+}
