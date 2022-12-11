714eea29b3d42bac09b436c3f32eded0d54e93aa
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2022-12-11 20:28:30 +0300

    Generic Repository Added in DataAccessLayer / Added

---
+++ DataAccessLayer/Repository/GenericRepository.cs
@@ -0,0 +1,43 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repository
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        Context context = new();
+
+        public void Delete(T t)
+        {
+            context.Remove(t);
+            context.SaveChanges();
+        }
+
+        public T GetByID(int id)
+        {
+            return context.Set<T>().Find(id);
+        }
+
+        public List<T> GetListAll()
+        {
+            return context.Set<T>().ToList();
+        }
+
+        public void Insert(T t)
+        {
+            context.Add(t);
+            context.SaveChanges();
+        }
+
+        public void Update(T t)
+        {
+            context.Update(t);
+            context.SaveChanges();
+        }
+    }
+}
