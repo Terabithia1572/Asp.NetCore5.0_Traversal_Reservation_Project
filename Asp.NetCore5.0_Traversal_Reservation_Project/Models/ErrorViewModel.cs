eee25e382c8bc3622304a2d4f2b79744e281fe7b
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2022-12-11 16:50:28 +0300

    Add project files.

---
+++ Asp.NetCore5.0_Traversal_Reservation_Project/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Asp.NetCore5._0_Traversal_Reservation_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
