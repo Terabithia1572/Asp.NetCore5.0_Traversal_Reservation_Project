d4908800df8b47972d053160fa1dc75058b9f3c3
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2022-12-12 19:48:39 +0300

    Fluent Validation Add in Business Layer + Theme Added / Added

---
+++ BusinessLayer/ValidationVules/AboutValidator.cs
@@ -0,0 +1,20 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationVules
+{
+    public class AboutValidator:AbstractValidator<About>
+    {
+        public AboutValidator()
+        {
+            RuleFor(x => x.AboutDescription).NotEmpty().WithMessage("Açıklama Kısmını Boş Geçemezsiniz...");
+            RuleFor(x => x.AboutImage1).NotEmpty().WithMessage("Lütfen Resim Kısmını Boş Geçemezsiniz..");
+            RuleFor(x => x.AboutDescription).MinimumLength(10).WithMessage("Lütfen En Az 10 Karakter Giriniz..");
+        }
+    }
+}
