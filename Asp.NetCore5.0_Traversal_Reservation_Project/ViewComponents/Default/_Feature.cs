794fcc990fb5a84cc052419778cebda7351dbc75
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2022-12-13 20:59:07 +0300

    Feature To Fill in Business Layer + List Feature / Added

---
+++ Asp.NetCore5.0_Traversal_Reservation_Project/ViewComponents/Default/_Feature.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_Traversal_Reservation_Project.ViewComponents.Default
+{
+    public class _Feature:ViewComponent
+    {
+        FeatureManager featureManager = new(new EfFeatureRepository());
+        public IViewComponentResult Invoke()
+        {
+            var values = featureManager.TGetList();
+            return View(values);
+        }
+    }
+}
---
+++ Asp.NetCore5.0_Traversal_Reservation_Project/ViewComponents/Default/_Statistics.cs
@@ -0,0 +1,26 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_Traversal_Reservation_Project.ViewComponents.Default
+{
+    public class _Statistics:ViewComponent
+    {
+        Context context = new();
+        
+        public IViewComponentResult Invoke()
+        {
+            Random r = new Random();
+            int sayi1 = r.Next(1, 999);
+            int sayi2 = r.Next(1, 999);
+           ViewBag.destinationsCount= context.Destinations.Count();
+            ViewBag.travelGuide = context.Guides.Count();
+            ViewBag.happyCustomer = sayi1;
+            ViewBag.Awards = sayi2;
+            return View();
+        }
+    }
+}
