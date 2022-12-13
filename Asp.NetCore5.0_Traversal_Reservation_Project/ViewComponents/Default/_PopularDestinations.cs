ac52b306d2b5ba728fffdd529065706a6f5d5786
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2022-12-13 19:02:57 +0300

    List Of Destination Added + Manager To Fill in Business Layer / Added

---
+++ Asp.NetCore5.0_Traversal_Reservation_Project/ViewComponents/Default/_PopularDestinations.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_Traversal_Reservation_Project.ViewComponents.Default
+{
+    public class _PopularDestinations:ViewComponent
+    {
+        DestinationManager destinationManager = new(new EfDestinationRepository());
+        public IViewComponentResult Invoke()
+        {
+            var values = destinationManager.TGetList();
+            return View(values);
+        }
+    }
+}
---
+++ BusinessLayer/Concrete/DestinationManager.cs
@@ -0,0 +1,47 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class DestinationManager : IDestinationService
+    {
+
+        IDestinationDal _destinationDal;
+
+        public DestinationManager(IDestinationDal destinationDal)
+        {
+            _destinationDal = destinationDal;
+        }
+
+        public Destination GetByID(int id)
+        {
+            return _destinationDal.GetByID(id);
+        }
+
+        public void TAdd(Destination t)
+        {
+            _destinationDal.Insert(t);
+        }
+
+        public void TDelete(Destination t)
+        {
+            _destinationDal.Delete(t);
+        }
+
+        public List<Destination> TGetList()
+        {
+            return _destinationDal.GetListAll();
+        }
+
+        public void TUpdate(Destination t)
+        {
+            _destinationDal.Update(t);
+        }
+    }
+}
