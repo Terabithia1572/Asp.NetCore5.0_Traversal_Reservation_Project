949b29a29719f6a9a66d07f3bbb52b9e30349331
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2022-12-14 20:45:59 +0300

    Layout Updated + Single Page Details / Added

--- Asp.NetCore5.0_Traversal_Reservation_Project/Controllers/DestinationController.cs
+++ Asp.NetCore5.0_Traversal_Reservation_Project/Controllers/DestinationController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -15,6 +16,17 @@ namespace Asp.NetCore5._0_Traversal_Reservation_Project.Controllers
         {
             var values = destinationManager.TGetList();
             return View(values);
+        }
+        [HttpGet]
+        public IActionResult DestinationDetails(int id)
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult DestinationDetails(Destination destination)
+        {
+            return View();
         }
     }
 }
