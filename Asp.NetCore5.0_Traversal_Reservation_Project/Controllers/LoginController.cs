7dc96a27c1b8ed0e37d2072fd099c16be97cf403
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2022-12-18 21:14:58 +0300

    Login And Register Pages + a Tag Css Coding Update / Added

---
+++ Asp.NetCore5.0_Traversal_Reservation_Project/Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_Traversal_Reservation_Project.Controllers
+{
+    [AllowAnonymous]
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        //[HttpPost]
+
+        //public IActionResult SignUp()
+        //{
+        //    return View();
+        //}
+
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
