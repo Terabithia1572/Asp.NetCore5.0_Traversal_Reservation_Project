7517c6ce65a9afaf3210ed797c6440dcc436f5f6
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2022-12-18 18:39:13 +0300

    Add Comment Process + Page Preparation/ Added

---
+++ Asp.NetCore5.0_Traversal_Reservation_Project/Controllers/CommentController.cs
@@ -0,0 +1,29 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_Traversal_Reservation_Project.Controllers
+{
+    public class CommentController : Controller
+    {
+        CommentManager commentManager = new CommentManager(new EfCommentRepository());
+        [HttpGet]
+        public PartialViewResult AddComment()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public PartialViewResult AddComment(Comment comment)
+        {
+            comment.CommentDate = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            comment.CommentStatus = true;
+            commentManager.TAdd(comment);
+            return PartialView();
+        }
+    }
+}
