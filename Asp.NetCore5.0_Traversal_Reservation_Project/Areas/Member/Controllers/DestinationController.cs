16d90361bb0428f6ca409ab7ff46564536d22be5
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2022-12-19 22:51:24 +0300

    New Partial Added + Member Page Data Listing / Added

---
+++ Asp.NetCore5.0_Traversal_Reservation_Project/Areas/Member/Controllers/DestinationController.cs
@@ -0,0 +1,23 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_Traversal_Reservation_Project.Areas.Member.Controllers
+{
+    [Area("Member")]
+    [AllowAnonymous]
+    public class DestinationController : Controller
+    {
+        DestinationManager destinationManager = new(new EfDestinationRepository());
+        public IActionResult Index()
+        {
+            var values = destinationManager.TGetList();
+            return View(values);
+        }
+    }
+}
