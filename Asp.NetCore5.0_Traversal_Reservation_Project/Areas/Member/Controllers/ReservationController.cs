ceac1ce8c7352a9c4b858411ed7bf45e7e5640e6
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2022-12-21 21:01:43 +0300

    New Reservation Entity Added + New Reservation Page+Controller / Added

---
+++ Asp.NetCore5.0_Traversal_Reservation_Project/Areas/Member/Controllers/ReservationController.cs
@@ -0,0 +1,33 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_Traversal_Reservation_Project.Areas.Member.Controllers
+{
+    [Area("Member")]
+    public class ReservationController : Controller
+    {
+        public IActionResult MyCurrentReservation()
+        {
+            return View();
+        }
+        public IActionResult MyOldReservation()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult NewReservation()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult NewReservation(Reservation reservation)
+        {
+            return View();
+        }
+    }
+}
--- EntityLayer/Concrete/AppUser.cs
+++ EntityLayer/Concrete/AppUser.cs
@@ -13,6 +13,7 @@ namespace EntityLayer.Concrete
         public string Name { get; set; }
         public string Surname { get; set; }
         public string Gender { get; set; }
+        public List<Reservation> Reservations { get; set; }
 
     }
 }
