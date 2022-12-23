e048d1f7d8977164caa16e48b2880893cddd7eb4
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2022-12-23 21:30:21 +0300

    Datas Listing Dashboard Panel With Identity / Added

---
+++ Asp.NetCore5.0_Traversal_Reservation_Project/ViewComponents/MemberDashboard/_ProfileInformation.cs
@@ -0,0 +1,29 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_Traversal_Reservation_Project.ViewComponents.MemberDashboard
+{
+    public class _ProfileInformation:ViewComponent
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public _ProfileInformation(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task< IViewComponentResult> InvokeAsync()
+        {
+            var values =await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.userName = values.Name + " " + values.Surname;
+            ViewBag.Phone = values.PhoneNumber;
+            ViewBag.Mail = values.Email;
+            return View();
+        }
+    }
+}
