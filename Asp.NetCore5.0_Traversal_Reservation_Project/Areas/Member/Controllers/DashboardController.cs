a548ce641b3267630396530e0b6741f6631532b1
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2022-12-23 20:52:34 +0300

    Dashboard Panel Added + New Profile Page Added + Profile Information Get With Identity / Added

---
+++ Asp.NetCore5.0_Traversal_Reservation_Project/Areas/Member/Controllers/DashboardController.cs
@@ -0,0 +1,31 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_Traversal_Reservation_Project.Areas.Member.Controllers
+{
+    [Area("Member")]
+    [Route("Member/[controller]/[action]")]
+    public class DashboardController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public DashboardController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task< IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.userName = values.Name + " " + values.Surname;
+            ViewBag.User = values.UserName;
+            ViewBag.Image = values.ImageURL;
+            return View();
+        }
+    }
+}
