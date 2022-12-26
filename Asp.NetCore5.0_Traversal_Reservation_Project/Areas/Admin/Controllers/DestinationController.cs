29aa736dc76751ed2cc2008f8d36385c85f5000b
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2022-12-26 22:06:26 +0300

    Admin Destination Controller Add+Update+Remove Process Added + Destination Add Page / Added

--- Asp.NetCore5.0_Traversal_Reservation_Project/Areas/Admin/Controllers/DestinationController.cs
+++ Asp.NetCore5.0_Traversal_Reservation_Project/Areas/Admin/Controllers/DestinationController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +12,42 @@ namespace Asp.NetCore5._0_Traversal_Reservation_Project.Areas.Admin.Controllers
     [Area("Admin")]
     public class DestinationController : Controller
     {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationRepository());
         public IActionResult Index()
         {
+            var values = destinationManager.TGetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddDestination()
+        {
             return View();
+        }
+        [HttpPost]
+        public IActionResult AddDestination(Destination destination)
+        {
+            destinationManager.TAdd(destination);
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteDestination(int id)
+        {
+            var values = destinationManager.GetByID(id);
+            destinationManager.TDelete(values);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateDestination(int id)
+        {
+            var values = destinationManager.GetByID(id);
+            return View(values);
+        }
+        [HttpPost]
+
+        public IActionResult UpdateDestination(Destination destination)
+        {
+            destinationManager.TUpdate(destination);
+            return RedirectToAction("Index");
         }
     }
 }
