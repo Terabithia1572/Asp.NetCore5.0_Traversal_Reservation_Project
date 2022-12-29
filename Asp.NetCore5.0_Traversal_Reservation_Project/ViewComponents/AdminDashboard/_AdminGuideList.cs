9ee924f2b227d63aa8b4e130fe09c5f916e3b170
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2022-12-29 23:24:00 +0300

    Admin Dashboard Process To be Continue + New Partial +Components / Added + Updated

---
+++ Asp.NetCore5.0_Traversal_Reservation_Project/ViewComponents/AdminDashboard/_AdminGuideList.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace Asp.NetCore5._0_Traversal_Reservation_Project.ViewComponents.AdminDashboard
+{
+    public class _AdminGuideList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
