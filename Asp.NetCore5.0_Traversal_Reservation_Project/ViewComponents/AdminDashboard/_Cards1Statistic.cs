182c93ad30e6e9c36bbc4d07c5b5919eb434a4ee
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2022-12-29 20:16:50 +0300

    New Dasboard Added + Dashboard Controller Added + New Statistic Datas Added / Added+ Updated

---
+++ Asp.NetCore5.0_Traversal_Reservation_Project/ViewComponents/AdminDashboard/_Cards1Statistic.cs
@@ -0,0 +1,20 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_Traversal_Reservation_Project.ViewComponents.AdminDashboard
+{
+    public class _Cards1Statistic:ViewComponent
+    {
+        Context context = new();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.TurSayisi = context.Destinations.Count();
+            ViewBag.MisafirSayisi = context.Users.Count();
+            return View();
+        }
+    }
+}
