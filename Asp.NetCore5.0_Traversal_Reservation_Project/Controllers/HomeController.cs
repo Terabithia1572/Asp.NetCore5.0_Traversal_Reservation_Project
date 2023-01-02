fe972ef5cda83a3b57113da28d3667ce340b8307
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2023-01-02 19:52:17 +0300

    SeriLog Added + Contact Process Countinue / Added

--- Asp.NetCore5.0_Traversal_Reservation_Project/Controllers/HomeController.cs
+++ Asp.NetCore5.0_Traversal_Reservation_Project/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Asp.NetCore5._0_Traversal_Reservation_Project.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -9,6 +10,7 @@ using System.Threading.Tasks;
 
 namespace Asp.NetCore5._0_Traversal_Reservation_Project.Controllers
 {
+    [AllowAnonymous]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -20,11 +22,20 @@ namespace Asp.NetCore5._0_Traversal_Reservation_Project.Controllers
 
         public IActionResult Index()
         {
+            _logger.LogInformation("Index Sayfası Çağrıldı");
+            _logger.LogError("Error Sayfası Çağrıldı..");
             return View();
         }
 
         public IActionResult Privacy()
         {
+            _logger.LogInformation("Privacy Sayfası Çağrıldı");
+            return View();
+        }
+
+        public IActionResult Test()
+        {
+            _logger.LogInformation("Test Sayfası Çağrıldı..");
             return View();
         }
 
--- Asp.NetCore5.0_Traversal_Reservation_Project/Startup.cs
+++ Asp.NetCore5.0_Traversal_Reservation_Project/Startup.cs
@@ -14,8 +14,10 @@ using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -33,6 +35,13 @@ namespace Asp.NetCore5._0_Traversal_Reservation_Project
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddLogging(x =>
+            {
+                x.ClearProviders();
+                x.SetMinimumLevel(LogLevel.Debug);
+                x.AddDebug();
+            });
+
             services.AddDbContext<Context>();
             services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<Context>()
                 .AddErrorDescriber<CustomIdentityValidator>().AddEntityFrameworkStores<Context>(); // Identity Yapýlanmasýný ekleedik
@@ -54,8 +63,10 @@ namespace Asp.NetCore5._0_Traversal_Reservation_Project
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env,ILoggerFactory loggerFactory)
         {
+            var path = Directory.GetCurrentDirectory();
+            loggerFactory.AddFile($"{path}\\Logs\\Log1.txt");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
