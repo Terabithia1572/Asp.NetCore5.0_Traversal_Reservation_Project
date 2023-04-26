770f27fe674b7fef55009e472befb18e09f4bc06
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2023-04-26 16:18:35 +0300

    Mail Send Process / Added

---
+++ Asp.NetCore5.0_Traversal_Reservation_Project/Areas/Admin/Controllers/MailController.cs
@@ -0,0 +1,43 @@
+﻿using Asp.NetCore5._0_Traversal_Reservation_Project.Models;
+using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_Traversal_Reservation_Project.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MailController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(MailRequest mailRequest)
+        {
+            MimeMessage mimeMessage = new();
+            MailboxAddress mailboxAddressFrom = new MailboxAddress("Admin","terabithia187@gmail.com");
+            mimeMessage.From.Add(mailboxAddressFrom);
+
+            MailboxAddress mailboxAddressTo = new MailboxAddress("User",mailRequest.ReceiverMail);
+            mimeMessage.To.Add(mailboxAddressTo);
+
+            var bodyBuilder = new BodyBuilder();
+            bodyBuilder.TextBody = mailRequest.Body;
+            mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+            mimeMessage.Subject = mailRequest.Subject;
+            SmtpClient client = new SmtpClient();
+            client.Connect("smtp.gmail.com", 587, false);
+            client.Authenticate("terabithia187@gmail.com", "cobccpeuztvhuceh");
+            client.Send(mimeMessage);
+            client.Disconnect(true);
+            return View();
+        }
+    }
+}
---
+++ Asp.NetCore5.0_Traversal_Reservation_Project/Models/MailRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_Traversal_Reservation_Project.Models
+{
+    public class MailRequest
+    {
+        public string SenderMail { get; set; }
+        public string ReceiverMail { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public string Name { get; set; }
+
+
+    }
+
+}
