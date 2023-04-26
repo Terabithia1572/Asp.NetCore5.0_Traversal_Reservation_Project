174cc80073e05cd2c206b022ec0c7e23aac44b37
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2023-04-26 18:05:57 +0300

    City + Ajax Process / Added

---
+++ Asp.NetCore5.0_Traversal_Reservation_Project/Areas/Admin/Controllers/CityController.cs
@@ -0,0 +1,48 @@
+﻿using Asp.NetCore5._0_Traversal_Reservation_Project.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_Traversal_Reservation_Project.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CityList()
+        {
+            var jsonCity = JsonConvert.SerializeObject(cities);
+            return Json(jsonCity);
+        }
+
+        public static List<CityClass> cities = new List<CityClass>
+    {
+        new CityClass
+        {
+            CityID=1,
+            CityName="Üsküp",
+            CityCountry="Makedonya"
+        },
+        new CityClass
+        {
+            CityID=2,
+            CityName="Roma",
+            CityCountry="İtalya"
+        },
+        new CityClass
+        {
+            CityID=3,
+            CityName="İstanbul",
+            CityCountry="Türkiye"
+        }
+     };
+    }
+}
+
