b9b65206ee47aee69bdeb2d0e59749d2b662e83b
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2023-04-27 16:16:25 +0300

    Contact US Added + Data Transfer Object (DTO) 2 / Added

---
+++ BusinessLayer/ValidationVules/AppUserRegisterValidator.cs
@@ -0,0 +1,26 @@
+﻿using DTOLayer.DTOs.AppUserDTOs;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationVules
+{
+    public class AppUserRegisterValidator:AbstractValidator<AppUserRegisterDTOs>
+    {
+        public AppUserRegisterValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Ad Alanı Boş Geçilemez..");
+            RuleFor(x => x.Surname).NotEmpty().WithMessage("SoyadAd Alanı Boş Geçilemez..");
+            RuleFor(x => x.Mail).NotEmpty().WithMessage("Mail Alanı Boş Geçilemez..");
+            RuleFor(x => x.Username).NotEmpty().WithMessage("Kullanıcı Adı Alanı Boş Geçilemez..");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Şifre Alanı Boş Geçilemez..");
+            RuleFor(x => x.ConfirmPassword).NotEmpty().WithMessage("Şifre Tekrar Alanı Boş Geçilemez..");
+            RuleFor(x => x.Username).MinimumLength(5).WithMessage("Lütfen En Az 5 Karakter Veri Girişi Yapınız..");
+            RuleFor(x => x.Username).MaximumLength(25).WithMessage("Lütfen En Fazla 25 Karakter Veri Girişi Yapınız..");
+            RuleFor(x => x.Password).Equal(y => y.ConfirmPassword).WithMessage("Şifreler Birbiriyle Uyuşmuyor..");
+        }
+    }
+}
