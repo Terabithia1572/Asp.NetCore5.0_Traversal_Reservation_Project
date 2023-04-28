f86aa372529f7f64f3a95183b85aed7800867e05
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2023-04-28 13:27:04 +0300

    Announcement Process + Announcement DTOs / Added

---
+++ Asp.NetCore5.0_Traversal_Reservation_Project/Areas/Admin/Controllers/AnnouncementController.cs
@@ -0,0 +1,37 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_Traversal_Reservation_Project.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AnnouncementController : Controller
+    {
+        private readonly IAnnouncementService _announcementService;
+
+        public AnnouncementController(IAnnouncementService announcementService)
+        {
+            _announcementService = announcementService;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _announcementService.TGetList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddAnnouncement()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddAnnouncement(string x)
+        {
+            return View();
+        }
+    }
+}
---
+++ BusinessLayer/ValidationVules/AnnouncementValidator.cs
@@ -0,0 +1,23 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationVules
+{
+    public class AnnouncementValidator:AbstractValidator<Announcement>
+    {
+        public AnnouncementValidator()
+        {
+            RuleFor(x => x.Title).NotEmpty().WithMessage("Lütfen Başlığı Boş Bırakmayınız..");
+            RuleFor(x => x.Content).NotEmpty().WithMessage("Lütfen Duyuru İçeriğini Boş Bırakmayınız..");
+            RuleFor(x => x.Title).MinimumLength(5).WithMessage("Lütfen En Az 5 Karakter Yazın...");
+            RuleFor(x => x.Content).MinimumLength(5).WithMessage("Lütfen En Az 5 Karakter Yazın...");
+            RuleFor(x => x.Content).MaximumLength(500).WithMessage("Lütfen En Fazla 500 Karakter Yazın...");
+            RuleFor(x => x.Title).MaximumLength(50).WithMessage("Lütfen En Fazla 50 Karakter Yazın...");
+        }
+    }
+}
