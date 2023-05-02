ff5599a67161313e7379f0af655c9d339cad8e05
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2023-05-02 15:48:56 +0300

    Api Movie Add / Top 100 IDMB Film List With Api / Added

---
+++ Asp.NetCore5.0_Traversal_Reservation_Project/Areas/Admin/Controllers/ApiMovieController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using Asp.NetCore5._0_Traversal_Reservation_Project.Areas.Admin.Models;
+using Newtonsoft.Json;
+
+namespace Asp.NetCore5._0_Traversal_Reservation_Project.Areas.Admin.Controllers
+{
+    public class ApiMovieController : Controller
+    {
+        [Area("Admin")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Index()
+        {
+			List<ApiMovieViewModel> apiMovies = new();
+			var client = new HttpClient();
+			var request = new HttpRequestMessage
+			{
+				Method = HttpMethod.Get,
+				RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/"),
+				Headers =
+	{
+		{ "X-RapidAPI-Key", "44a9b3ca28msh4dc1bc7bb828d5ap18c256jsn9125f1e0404c" },
+		{ "X-RapidAPI-Host", "imdb-top-100-movies.p.rapidapi.com" },
+	},
+			};
+			using (var response = await client.SendAsync(request))
+			{
+				response.EnsureSuccessStatusCode();
+				var body = await response.Content.ReadAsStringAsync();
+				apiMovies = JsonConvert.DeserializeObject<List<ApiMovieViewModel>>(body);
+				return View(apiMovies);
+			}
+		}
+    }
+}
