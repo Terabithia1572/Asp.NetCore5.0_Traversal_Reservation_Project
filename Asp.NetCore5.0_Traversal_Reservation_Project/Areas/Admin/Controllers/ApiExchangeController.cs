3f86880e6802c9ac7a55e035fa45d68ef156d399
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2023-05-02 16:41:34 +0300

    Rapid Api & Booking Exchange Api / Added

---
+++ Asp.NetCore5.0_Traversal_Reservation_Project/Areas/Admin/Controllers/ApiExchangeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using Asp.NetCore5._0_Traversal_Reservation_Project.Areas.Admin.Models;
+using Newtonsoft.Json;
+
+namespace Asp.NetCore5._0_Traversal_Reservation_Project.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [AllowAnonymous]
+    public class ApiExchangeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+			List <BookingExchangeViewModel2> bookingExchangeViewModels= new();
+			var client = new HttpClient();
+			var request = new HttpRequestMessage
+			{
+				Method = HttpMethod.Get,
+				RequestUri = new Uri("https://booking-com.p.rapidapi.com/v1/metadata/exchange-rates?currency=TRY&locale=en-gb"),
+				Headers =
+	{
+		{ "X-RapidAPI-Key", "44a9b3ca28msh4dc1bc7bb828d5ap18c256jsn9125f1e0404c" },
+		{ "X-RapidAPI-Host", "booking-com.p.rapidapi.com" },
+	},
+			};
+			using (var response = await client.SendAsync(request))
+			{
+				response.EnsureSuccessStatusCode();
+				var body = await response.Content.ReadAsStringAsync();
+			var values= JsonConvert.DeserializeObject<BookingExchangeViewModel2>(body);
+				return View(values.exchange_rates);
+			}
+		}
+    }
+}
---
+++ Asp.NetCore5.0_Traversal_Reservation_Project/Areas/Admin/Models/BookingExchangeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_Traversal_Reservation_Project.Areas.Admin.Models
+{
+    public class BookingExchangeViewModel
+    {
+        public string currency { get; set; }
+        public string exchange_rate_buy { get; set; }
+    }
+}
