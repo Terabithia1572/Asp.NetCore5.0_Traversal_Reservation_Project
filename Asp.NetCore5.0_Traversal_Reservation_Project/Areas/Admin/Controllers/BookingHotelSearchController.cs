65200a38321f840c27e8919c86d13f23536d36a4
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2023-05-03 13:21:47 +0300

    BookingSearch Hotel API Added + Hotel List Page With API / Added

---
+++ Asp.NetCore5.0_Traversal_Reservation_Project/Areas/Admin/Controllers/BookingHotelSearchController.cs
@@ -0,0 +1,40 @@
+﻿using Asp.NetCore5._0_Traversal_Reservation_Project.Areas.Admin.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_Traversal_Reservation_Project.Areas.Admin.Controllers
+{
+    public class BookingHotelSearchController : Controller
+    {
+		[AllowAnonymous]
+		[Area("Admin")]
+        public async Task<IActionResult> Index()
+        {
+			var client = new HttpClient();
+			var request = new HttpRequestMessage
+			{
+				Method = HttpMethod.Get,
+				RequestUri = new Uri("https://booking-com.p.rapidapi.com/v2/hotels/search?order_by=popularity&adults_number=2&checkin_date=2024-06-27&filter_by_currency=EUR&dest_id=-2601889&locale=en-gb&checkout_date=2024-07-28&units=metric&room_number=1&dest_type=city&include_adjacency=true&children_number=2&page_number=0&children_ages=5%2C0&categories_filter_ids=class%3A%3A2%2Cclass%3A%3A4%2Cfree_cancellation%3A%3A1"),
+				Headers =
+	{
+		{ "X-RapidAPI-Key", "44a9b3ca28msh4dc1bc7bb828d5ap18c256jsn9125f1e0404c" },
+		{ "X-RapidAPI-Host", "booking-com.p.rapidapi.com" },
+	},
+			};
+			using (var response = await client.SendAsync(request))
+			{
+				response.EnsureSuccessStatusCode();
+				var body = await response.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<BookingHotelSearchViewModel>(body);
+				return View(values.results);
+			}
+			
+        }
+    }
+}
