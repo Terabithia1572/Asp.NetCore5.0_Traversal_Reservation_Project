75ab575a3e5e831d75633800f0be49ab85ef809d
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2023-05-04 13:36:31 +0300

    Account All Layer Added + Unit Of Work Implements Design Pattern / Added

---
+++ BusinessLayer/Abstract/AbstractUow/IGenericUowService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract.AbstractUow
+{
+   public interface IGenericUowService<T>
+    {
+        void TAdd(T t);
+        void TUpdate(T t);
+        void TMultiUpdate(List<T> t);
+    }
+}
---
+++ DataAccessLayer/UniOfWork/UowDal.cs
@@ -0,0 +1,24 @@
+﻿using DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.UniOfWork
+{
+    public class UowDal : IUowDal
+    {
+        private readonly Context _context;
+
+        public UowDal(Context context)
+        {
+            _context = context;
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
