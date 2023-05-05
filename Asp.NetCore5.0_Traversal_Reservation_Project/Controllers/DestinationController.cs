5fe4df9f89b4294154bf48207478d11410da1196
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2023-05-05 16:40:29 +0300

    Designal Editing + Destination Design Page / Editing

--- Asp.NetCore5.0_Traversal_Reservation_Project/Controllers/DestinationController.cs
+++ Asp.NetCore5.0_Traversal_Reservation_Project/Controllers/DestinationController.cs
@@ -1,6 +1,7 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -9,6 +10,7 @@ using System.Threading.Tasks;
 
 namespace Asp.NetCore5._0_Traversal_Reservation_Project.Controllers
 {
+    [AllowAnonymous]
     public class DestinationController : Controller
     {
         DestinationManager destinationManager = new(new EfDestinationRepository());
