2f013801e834023985c781f7a1cd70600df0151f
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2023-05-09 15:38:54 +0300

    Guide List + Guide Page / Added

---
+++ Asp.NetCore5.0_Traversal_Reservation_Project/Controllers/GuideController.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_Traversal_Reservation_Project.Controllers
+{
+    [AllowAnonymous]
+    public class GuideController : Controller
+    {
+        GuideManager guideManager = new(new EfGuideRepository());
+        public IActionResult Index()
+        {
+            var values = guideManager.TGetList();
+            return View(values);
+        }
+    }
+}
