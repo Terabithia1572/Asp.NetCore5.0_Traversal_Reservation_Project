3b6f72f4670b0d97b1f4fe444992257471e26605
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2023-05-09 15:49:59 +0300

    Contact Us Page / Added

---
+++ Asp.NetCore5.0_Traversal_Reservation_Project/Controllers/ContactController.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_Traversal_Reservation_Project.Controllers
+{
+    [AllowAnonymous]
+    public class ContactController : Controller
+    {
+        ContactUsManager contactUsManager = new(new EfContactUsRepository());
+        public IActionResult Index()
+        {
+            var values = contactUsManager.TGetList();
+            return View();
+        }
+    }
+}
